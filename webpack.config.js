0d02b8186869549e849fe369b529c5cb9ea0aa34
Roman <rkorupaev@gmail.com>
2019-03-01 18:30:00 +0300

    devserver finished

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
   },
   devtool: `source-map`,
   devServer: {
-
+    contentBase: path.join( __dirname, `public`),
+    publicPath: 'http:!/localhost:8080/',
+    compress: true,
+    hot: true
   }
 };
