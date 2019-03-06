62a86c055086f4429e01a85aeccc181769529fd6
Roman <rkorupaev@gmail.com>
2019-03-06 20:46:43 +0300

    1-7 homework

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
 import makePoint from './make-point.js';
 import makeFilter from './make-filter.js';
 
-
-console.log('test');
-
-
 const filterContainer = document.querySelector(`.trip-filter`);
-
-filterContainer.insertAdjacentHTML(`beforeend`, makeFilter(test));
-
-
-
+const filters = [`Everything`, `Future`, `Past`];
+filters.forEach(function(element) {
+  filterContainer.insertAdjacentHTML(`beforeend`, makeFilter(element));
+});
 
 const dayItems = document.querySelector(`.trip-day__items`);
-
-dayItems.insertAdjacentHTML(`beforeend`, makePoint);
+for (let i = 0; i < 7; i++) {
+  dayItems.insertAdjacentHTML(`beforeend`, makePoint());
+};
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require (`path`);
 
 module.exports = {
   mode: `development`,
-  entry: `src/main.js`,
+  entry: `./src/main.js`,
   output: {
     filename: `bundle.js`,
     path: path.join ( __dirname, `public`)
