642f8e14670efb194f14317bc56bb39d39a75414
Roman <rkorupaev@gmail.com>
2019-03-16 17:10:07 +0300

    task 1

---
+++ src/class-point.js
@@ -0,0 +1,11 @@
+class Point = {
+  constructor (data) {
+      this._town: data.town;
+      this._photo: data.photo;
+      this._icon: data.icon;
+      this._title: data.title;
+      this._time: data.time;
+      this._price: data.price;
+      this._offer: data.offer;
+  };
+};
